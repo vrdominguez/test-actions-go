acee9d376d9cee66cc31598e14ce72b32c0362c7
Víctor R. Rodríguez Domínguez <victor.rodriguez@prefapp.es>
2022-11-22 10:01:33 +0100

    Added simple go rutine

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
 	fmt.Printf("Current OS: %s\n", getOS())
+
+	channel := make(chan bool)
+
+	// Wait for goruine to end
+	go simpleWait(channel)
+	fmt.Println("Launched gorutine, waiting...")
+	<-channel
+	fmt.Println("Finished gorutine")
 }
 
 func getOS() string {
 	return runtime.GOOS
+}
+
+func simpleWait(finish chan bool) {
+	time.Sleep(5 * time.Second)
+
+	finish <- true
 }
